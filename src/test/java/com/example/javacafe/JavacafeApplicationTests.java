b1ca9857bc2d9eb22be6ce5f4ddc15d8abc37f4d
klajdshkalla <klajdshkalla@gmail.com>
2025-02-02 10:50:51 +0100

    first commit

---
+++ src/test/java/com/example/javacafe/JavacafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javacafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavacafeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
