c8503c2a31b2dbec1699d28812da72af5696a8d7
Klajd Shkalla <klajdshkalla@gmail.com>
2025-02-15 17:37:55 +0100

    FOTO-RREGULLIME

--- src/main/java/com/example/javacafe/repositories/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.javacafe.repositories;
-
-import com.example.javacafe.entities.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
